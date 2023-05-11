a99539dec01d4b470f86d99e9328da3bd186c9fc
AndyFAN <48795238+AndyFAN1996@users.noreply.github.com>
2023-05-11 15:00:38 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ def hello(i):
     # classifier = pipeline("sentiment-analysis")
     # a = classifier(i)
     # return a
-    a = 1/0
+#     a = 1/0
     return "hello world!"
-a = 1/0
+# a = 1/0
 iface = gr.Interface(fn=hello, inputs="text", outputs="text")
 iface.launch(server_name="0.0.0.0")
